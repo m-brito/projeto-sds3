07d137cf5eefcff1443ba95b428b5b59b6f9375a
Mauricio Brito <britomauricio2003@gmail.com>
2021-09-30 14:33:56 -0300

    Componentes estaticos basicos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/mauricio-teixeira-37a932196/" target="_blank" rel="noreferrer">Maurício Brito Teixeira</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
