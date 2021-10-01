a4d747bc4accb9b134fc645b5731941557f30367
Mauricio Brito <britomauricio2003@gmail.com>
2021-10-01 08:02:52 -0300

    Rotas

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="footer mt-auto py-3 bg-dark">
+        <footer className="footer mt-auto py-4 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/mauricio-teixeira-37a932196/" target="_blank" rel="noreferrer">Maurício Brito Teixeira</a></p>
+                <p className="text-light mb-0">App desenvolvido por <a href="https://www.linkedin.com/in/mauricio-teixeira-37a932196/" target="_blank" rel="noreferrer">Maurício Brito Teixeira</a></p>
             </div>
         </footer>
     )
